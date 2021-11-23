15c5ff3ef915cb88595883b8fdbb83a6667a72f1
mohamadsaffari90@gmail.com <mohamadsaffari90@gmail.com>
2021-11-23 15:36:42 +0330

    Add Header component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Header from "header/Header";
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+  <div>
+    <Header />
+    Hi there, I'm Home App. <h1>Home.</h1>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath:
+      argv.mode == "development"
+        ? "http://localhost:8081/"
+        : "https://prod-mf-home.vercel.app/",
   },
 
   resolve: {
@@ -12,7 +15,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 8080,
+    port: 8081,
   },
 
   module: {
@@ -40,9 +43,14 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "starter",
+      name: "home",
       filename: "remoteEntry.js",
-      remotes: {},
+      remotes: {
+        header:
+          argv.mode == "development"
+            ? "header@http://localhost:8080/remoteEntry.js"
+            : "header@https://prod-mf-header.vercel.app/remoteEntry.js",
+      },
       exposes: {},
       shared: {
         ...deps,
@@ -60,4 +68,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
